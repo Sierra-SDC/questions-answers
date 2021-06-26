1fb47a7113e2443da2e52b1bcf330e93d175fed1
lerisse <tristanlerisse@gmail.com>
2021-06-26 15:48:21 -0400

    Added Index to db creation

--- server/db/queries/schema.sql
+++ server/db/queries/schema.sql
@@ -1,7 +1,7 @@
 
-DROP DATABASE IF EXISTS qa_db;
+DROP DATABASE IF EXISTS qa_db_test;
 
-CREATE DATABASE qa_db;
+CREATE DATABASE qa_db_test;
 
 CREATE TABLE IF NOT EXISTS public.questions
 (
@@ -17,9 +17,10 @@ CREATE TABLE IF NOT EXISTS public.questions
     PRIMARY KEY (id)
 );
 
--- /copy questions (id, product_id, body, date_written, asker_name, asker_email, reported, helpful)
--- FROM './csv/questions.csv'
--- DELIMITER ',' CSV HEADER;
+COPY questions (id, product_id, body, date_written, asker_name, asker_email, reported, helpful)
+FROM './csv/questions.csv'
+DELIMITER ','
+CSV HEADER;
 
 CREATE TABLE IF NOT EXISTS public.answers
 (
@@ -34,9 +35,10 @@ CREATE TABLE IF NOT EXISTS public.answers
     PRIMARY KEY (id)
 );
 
--- /copy answers (id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful)
--- FROM './csv/answers.csv'
--- DELIMITER ',' CSV HEADER;
+COPY answers (id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful)
+FROM './csv/answers.csv'
+DELIMITER ','
+CSV HEADER;
 
 CREATE TABLE IF NOT EXISTS public.photos
 (
@@ -46,9 +48,10 @@ CREATE TABLE IF NOT EXISTS public.photos
     PRIMARY KEY (id)
 );
 
--- /copy photos (id, answer_id, url)
--- FROM './csv/answers_photos.csv'
--- DELIMITER ',' CSV HEADER;
+COPY photos (id, answer_id, url)
+FROM './csv/answers_photos.csv'
+DELIMITER ','
+CSV HEADER;
 
 ALTER TABLE public.answers
     ADD FOREIGN KEY (question_id)
@@ -60,3 +63,7 @@ ALTER TABLE public.photos
     ADD FOREIGN KEY (answer_id)
     REFERENCES public.answers (id)
     NOT VALID;
+
+CREATE UNIQUE INDEX questions_idx ON questions (product_id);
+CREATE UNIQUE INDEX answers_idx ON answers (questions_id);
+CREATE UNIQUE INDEX photos_idx ON photos (answers_id);
