8497fda21bb094b9f44de1367e091f8cf0260015
lerisse <tristanlerisse@gmail.com>
2021-06-26 15:17:46 -0400

    Update Schema

--- server/db/queries/schema.sql
+++ server/db/queries/schema.sql
@@ -3,43 +3,60 @@ DROP DATABASE IF EXISTS qa_db;
 
 CREATE DATABASE qa_db;
 
-CREATE TABLE questions (
-  id SERIAL PRIMARY KEY,
-  product_id INT,
-  body VARCHAR(1000) NOT NULL,
-  date_written VARCHAR(100) DEFAULT EXTRACT(EPOCH FROM NOW()),
-  asker_name VARCHAR(50) NOT NULL,
-  asker_email VARCHAR(100) NOT NULL,
-  reported INT DEFAULT 0,
-  helpful INT DEFAULT 0
+CREATE TABLE IF NOT EXISTS public.questions
+(
+    id integer NOT NULL,
+    product_id integer,
+    body character varying(1000) NOT NULL,
+    date_written character varying(100),
+    asker_name character varying(50) NOT NULL,
+    asker_email character varying(100) NOT NULL,
+    reported integer,
+    helpful integer,
+    answers "char",
+    PRIMARY KEY (id)
 );
 
 -- /copy questions (id, product_id, body, date_written, asker_name, asker_email, reported, helpful)
 -- FROM './csv/questions.csv'
 -- DELIMITER ',' CSV HEADER;
 
-CREATE TABLE answers (
-  id SERIAL PRIMARY KEY,
-  question_id INT,
-  body VARCHAR(1000) NOT NULL,
-  date_written VARCHAR(100),
-  answerer_name VARCHAR(50) NOT NULL,
-  answerer_email VARCHAR(100) NOT NULL,
-  reported INT DEFAULT 0,
-  helpful INT DEFAULT 0,
+CREATE TABLE IF NOT EXISTS public.answers
+(
+    id integer NOT NULL,
+    question_id integer,
+    body character varying(1000) NOT NULL,
+    date_written character varying(100),
+    answerer_name character varying(50) NOT NULL,
+    answerer_email character varying(100) NOT NULL,
+    reported integer,
+    helpful integer,
+    PRIMARY KEY (id)
 );
 
 -- /copy answers (id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful)
 -- FROM './csv/answers.csv'
 -- DELIMITER ',' CSV HEADER;
 
-CREATE TABLE photos (
-  id SERIAL PRIMARY KEY,
-  answer_id INT,
-  url VARCHAR(500) NOT NULL,
+CREATE TABLE IF NOT EXISTS public.photos
+(
+    id integer NOT NULL,
+    answer_id integer,
+    url character varying(500) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 -- /copy photos (id, answer_id, url)
 -- FROM './csv/answers_photos.csv'
 -- DELIMITER ',' CSV HEADER;
 
+ALTER TABLE public.answers
+    ADD FOREIGN KEY (question_id)
+    REFERENCES public.questions (id)
+    NOT VALID;
+
+
+ALTER TABLE public.photos
+    ADD FOREIGN KEY (answer_id)
+    REFERENCES public.answers (id)
+    NOT VALID;
