8510e14108ded21c91c8991c365328687fa111f7
lerisse <tristanlerisse@gmail.com>
2021-06-26 22:32:38 -0400

    Late night update

--- server/db/queries/schema.sql
+++ server/db/queries/schema.sql
@@ -38,17 +38,6 @@ CREATE TABLE IF NOT EXISTS public.photos
     PRIMARY KEY (id)
 );
 
-ALTER TABLE public.answers
-    ADD FOREIGN KEY (question_id)
-    REFERENCES public.questions (id)
-    NOT VALID;
-
-
-ALTER TABLE public.photos
-    ADD FOREIGN KEY (answer_id)
-    REFERENCES public.answers (id)
-    NOT VALID;
-
 
 COPY questions (id, product_id, body, date_written, asker_name, asker_email, reported, helpful)
 FROM '/home/tristan/HackReactor/SDC/questions-answers/csv/questions.csv'
@@ -64,6 +53,21 @@ COPY photos (id, answer_id, url)
 FROM '/home/tristan/HackReactor/SDC/questions-answers/csv/answers_photos.csv'
 DELIMITER ','
 CSV HEADER;
+
+
+ALTER TABLE public.answers
+    ADD FOREIGN KEY (question_id)
+    REFERENCES public.questions (id)
+    NOT VALID;
+
+
+ALTER TABLE public.photos
+    ADD FOREIGN KEY (answer_id)
+    REFERENCES public.answers (id)
+    NOT VALID;
+
+
+
 
 CREATE UNIQUE INDEX questions_idx ON questions (product_id);
 CREATE UNIQUE INDEX answers_idx ON answers (questions_id);
